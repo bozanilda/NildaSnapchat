49ae0ed13158e87df47a6fdddef66c5c043ad647
bozanilda <nilda.boza@tecsup.edu.pe>
2024-11-15 09:55:31 -0500

    Initial Commit

---
+++ NildaSnapchatTests/NildaSnapchatTests.swift
@@ -0,0 +1,17 @@
+//
+//  NildaSnapchatTests.swift
+//  NildaSnapchatTests
+//
+//  Created by Nilda Boza on 15/11/24.
+//
+
+import Testing
+@testable import NildaSnapchat
+
+struct NildaSnapchatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
